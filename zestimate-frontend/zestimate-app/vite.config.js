a9c3c7ce988383f70349da47b52a7b76e27c9ce8
George Borrero <georgeborrero@Georges-MBP.lan>
2024-09-15 20:49:38 -0700

    vite.js

--- zestimate-frontend/zestimate-app/vite.config.js
+++ zestimate-frontend/zestimate-app/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/repositories/zestimate_api/zestimate-frontend/zestimate-app/', // Reflects your app's subdirectory
 })
+
+
+  
