3838e93760b661817e54855664ed2a760e87c648
George B <info@really.app>
2024-09-19 20:47:47 -0700

    vite.js

--- zestimate-frontend/zestimate-app/vite.config.js
+++ zestimate-frontend/zestimate-app/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'development' ? '/' : '/repositories/zestimate_api/zestimate-frontend/zestimate-app/',
-  build: {
-    outDir: 'dist', // Ensure the output directory is set to 'dist'
-  },
-}))
+  base: '/',
+})
